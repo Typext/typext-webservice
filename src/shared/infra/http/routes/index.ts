82c1f5bd71ef7473798d8f6f604c5fc085bb7e7f
EvandroRBR <evandro.braga01@fatec.sp.gov.br>
2021-04-01 08:48:43 -0300

    configuraçoes iniciais

---
+++ src/shared/infra/http/routes/index.ts
@@ -0,0 +1,8 @@
+import usersRouter from '@modules/users/infra/http/routes/user.routes';
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.use('/users', usersRouter);
+
+export default routes;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,10 +1,14 @@
 import 'reflect-metadata';
 
-import '@shared/infra/typeorm';
-
 import express from 'express';
 
+import routes from './routes';
+import '@shared/infra/typeorm';
+
 const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
