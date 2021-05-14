4f43074d91abd58ef8f0a9824ae4212f7515974a
JDanrley <jose.silva478@fatec.sp.gov.br>
2021-05-14 20:39:55 -0300

    Joi adicionado a rota de minutas

--- src/modules/minutes/infra/http/routes/minute.routes.ts
+++ src/modules/minutes/infra/http/routes/minute.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Segments, Joi } from 'celebrate';
 import { Router } from 'express';
 
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
@@ -10,7 +11,35 @@ const minutesController = new MinutesController();
 minuteRouter.use(ensureAuthenticated);
 
 minuteRouter.get('/:id', minutesController.show);
-minuteRouter.post('/', minutesController.create);
+minuteRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      minute: {
+        start_date: Joi.date().required(),
+        minute_number: Joi.string().alphanum().required(),
+        place: Joi.string().required(),
+        project: Joi.string().required(),
+        schedules: Joi.array().required(),
+        areas: Joi.array().required(),
+      },
+      participant: Joi.array().items({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        title: Joi.string().max(3).required(),
+        company: Joi.string().required(),
+        phone: Joi.string().required(),
+        digital_signature: Joi.bool().required(),
+      }),
+      topic: Joi.array().items({
+        name: Joi.string().min(2).required(),
+        responsible: Joi.string().required(),
+        deadline: Joi.date().required(),
+      }),
+    },
+  }),
+  minutesController.create,
+);
 minuteRouter.get('/', minutesController.index);
 
 export default minuteRouter;
