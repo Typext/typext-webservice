d13dd8fb98e8c8f3fc2d1a3c2215dc17edd2fbba
JDanrley <jose.silva478@fatec.sp.gov.br>
2021-05-15 17:09:34 -0300

    Celebrate aplicado a rota

--- src/modules/minutes/infra/http/routes/review.routes.ts
+++ src/modules/minutes/infra/http/routes/review.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-
+import { celebrate, Segments, Joi } from 'celebrate';
 import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
 import ReviewsController from '../controllers/ReviewsController';
 
@@ -9,6 +9,17 @@ const reviewsController = new ReviewsController();
 
 reviewRouter.use(ensureAuthenticated);
 
-reviewRouter.post('/', reviewsController.create);
+reviewRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      minute_id: Joi.string().required(),
+      topic: Joi.string().required(),
+      responsible: Joi.string().required(),
+      deadline: Joi.date().required(),
+    },
+  }),
+  reviewsController.create,
+);
 
 export default reviewRouter;
